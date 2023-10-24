3428077fe532d6f32bc23695056362aca2d1e2ab
zhihanyang2022 <yangz2@carleton.edu>
2023-10-24 17:33:25 +0800

    updated images for uncond vi

--- core.py
+++ core.py
@@ -2,12 +2,15 @@ import torch
 import torch.nn as nn
 import torch.distributions as pdist
 from torch.autograd.functional import jacobian
-m = lambda x : -1 + nn.functional.softplus(x)
 from torch import log, abs, sum
 
 
-def tanh_derivative(x):
+def dtanh(x):
     return 1 - (torch.tanh(x)) ** 2
+
+
+def m(x):
+    return -1 + nn.functional.softplus(x)
 
 
 class PlanarFlow(nn.Module):
@@ -20,48 +23,51 @@ class PlanarFlow(nn.Module):
 
     @staticmethod
     def logabsdet(z, w, u, b):
-        return log(abs(1 + (tanh_derivative(z @ w + b) * w.T) @ u.T)).view(-1)
+        return log(abs(1 + (dtanh(z @ w + b) * w.T) @ u.T)).view(-1)
 
-    def forward(self, z):
+    def forward(self, z, return_logabsdet=False):
         w_dot_u = (self.u @ self.w).squeeze()
         u_hat = self.u + (m(w_dot_u + 0.5413) - w_dot_u) * self.w.T / sum(self.w ** 2)
-        return z + u_hat * torch.tanh(z @ self.w + self.b), self.logabsdet(z, self.w, u_hat, self.b)
+        if return_logabsdet:
+            return z + u_hat * torch.tanh(z @ self.w + self.b), self.logabsdet(z, self.w, u_hat, self.b)
+        else:
+            return z + u_hat * torch.tanh(z @ self.w + self.b)
     
     
 def batch_jacobian(f, x):
-    f_sum = lambda x: torch.sum(f(x), axis=0)
+    f_sum = lambda x: torch.sum(f(x), dim=0)
     return jacobian(f_sum, x, create_graph=True).permute(1,0,2)
 
 
-class DiagonalGaussian(nn.Module):
-    
-    def __init__(self, D):
-        super().__init__()
-        self.μ = nn.Parameter(torch.zeros(D, ))
-        self.pre_σ = nn.Parameter(data=torch.zeros(D, ))
-        
-    def sample(self, n):
-        dist = pdist.multivariate_normal.MultivariateNormal(self.μ, torch.eye(D) * torch.exp(self.pre_σ))
-        samples = dist.rsample(sample_shape=(n, ))
-        return samples, dist.log_prob(samples)
-    
-    
-class FullRankGaussian(nn.Module):
-    
-    def __init__(self, D):
-        super().__init__()
-        self.μ = nn.Parameter(torch.zeros(D, ))
-        self.cov_decomp = nn.Parameter(torch.linalg.cholesky(torch.eye(D), upper=True))
-        
-    @property
-    def cov(self):
-        temp = torch.triu(self.cov_decomp)
-        return temp.T @ temp
-        
-    def sample(self, n):
-        dist = pdist.multivariate_normal.MultivariateNormal(self.μ, self.cov)
-        samples = dist.rsample(sample_shape=(n, ))
-        return samples, dist.log_prob(samples)
+# class DiagonalGaussian(nn.Module):
+#
+#     def __init__(self, D):
+#         super().__init__()
+#         self.μ = nn.Parameter(torch.zeros(D, ))
+#         self.pre_σ = nn.Parameter(data=torch.zeros(D, ))
+#
+#     def sample(self, n):
+#         dist = pdist.multivariate_normal.MultivariateNormal(self.μ, torch.eye(D) * torch.exp(self.pre_σ))
+#         samples = dist.rsample(sample_shape=(n, ))
+#         return samples, dist.log_prob(samples)
+#
+#
+# class FullRankGaussian(nn.Module):
+#
+#     def __init__(self, D):
+#         super().__init__()
+#         self.μ = nn.Parameter(torch.zeros(D, ))
+#         self.cov_decomp = nn.Parameter(torch.linalg.cholesky(torch.eye(D), upper=True))
+#
+#     @property
+#     def cov(self):
+#         temp = torch.triu(self.cov_decomp)
+#         return temp.T @ temp
+#
+#     def sample(self, n):
+#         dist = pdist.multivariate_normal.MultivariateNormal(self.μ, self.cov)
+#         samples = dist.rsample(sample_shape=(n, ))
+#         return samples, dist.log_prob(samples)
     
         
 class NormalizingFlow(nn.Module):
@@ -80,16 +86,16 @@ class NormalizingFlow(nn.Module):
         
     def sample_from_each_layer(self, n):
         with torch.no_grad():
-            z_0 = self.q_0.sample(sample_shape=(n, ))
+            z_0 = self.q_0.sample(sample_shape=torch.Size([n]))
             zs = [z_0]
             z = z_0
             for layer in self.layers:
-                z, _ = layer(z)
+                z = layer(z, return_logabsdet=False)
                 zs.append(z)
             return zs
     
     def sample(self, n, efficient=True):
-        z_0 = self.q_0.rsample(sample_shape=(n, ))  # use rsample so that we can train self.μ and self.pre_σ
+        z_0 = self.q_0.rsample(sample_shape=torch.Size([n]))  # use rsample so that we can train self.μ and self.pre_σ
         if not efficient:
             z_K = self.transformation(z_0)
             logabsdet_of_all_layers = torch.linalg.slogdet(batch_jacobian(self.transformation, z_0))[1]
@@ -98,7 +104,7 @@ class NormalizingFlow(nn.Module):
             z = z_0
             log_prob = self.q_0.log_prob(z_0)
             for layer in self.layers:
-                new_z, term = layer(z)
+                new_z, term = layer(z, return_logabsdet=True)
                 log_prob -= term
                 z = new_z
             return z, log_prob
